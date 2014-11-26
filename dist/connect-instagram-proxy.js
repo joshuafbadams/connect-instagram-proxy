cf2e0ea2e7ee2444e764c4b2b3f276fd3ce90a84
Vitor Mattos <vitor.mattos@vtex.com.br>
2014-11-26 17:22:34 -0200

    Middleware to get all pages from instagram api

--- dist/connect-instagram-proxy.js
+++ dist/connect-instagram-proxy.js
@@ -4,11 +4,20 @@ Module dependencies.
  */
 
 (function() {
-  var request;
+  var request, sendRequest, setHeaders;
 
   request = require('request');
 
-  module.exports = function(clientId, userId) {
+  sendRequest = function(url, callback) {
+    return request.get(url, callback);
+  };
+
+  setHeaders = function(res) {
+    res.setHeader("Content-Type", "application/json");
+    return res.setHeader("Access-Control-Allow-Origin", "*");
+  };
+
+  exports.firstPage = function(clientId, userId) {
     if (userId == null) {
       userId = '';
     }
@@ -16,12 +25,48 @@ Module dependencies.
       throw new Error('"clientId" parameter is required');
     }
     return function(req, res, next) {
-      return request.get("https://api.instagram.com/v1/users/" + userId + "/media/recent/?client_id=" + clientId, function(err, response, body) {
-        res.setHeader("Content-Type", "application/json");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.write(body);
+      return sendRequest("https://api.instagram.com/v1/users/" + userId + "/media/recent/?client_id=" + clientId, function(err, response, body) {
+        var resObj;
+        setHeaders(res);
+        resObj = JSON.parse(body);
+        res.write(JSON.stringify({
+          data: resObj.data
+        }));
         return res.end();
       });
+    };
+  };
+
+  exports.allPages = function(clientId, userId) {
+    if (userId == null) {
+      userId = '';
+    }
+    if (clientId == null) {
+      throw new Error('"clientId" parameter is required');
+    }
+    return function(req, res, next) {
+      var callback, data;
+      data = [];
+      callback = function(err, response, body) {
+        var paginationUrl, resObj;
+        resObj = JSON.parse(body);
+        paginationUrl = resObj.pagination['next_url'];
+        data = data.concat(resObj.data);
+        console.log('paginationurl:', paginationUrl);
+        if (paginationUrl != null) {
+          return sendRequest(paginationUrl, callback);
+        } else {
+          setHeaders(res);
+          console.log(JSON.stringify({
+            data: data
+          }));
+          res.write(JSON.stringify({
+            data: data
+          }));
+          return res.end();
+        }
+      };
+      return sendRequest("https://api.instagram.com/v1/users/" + userId + "/media/recent/?client_id=" + clientId, callback);
     };
   };
 
--- src/connect-instagram-proxy.coffee
+++ src/connect-instagram-proxy.coffee
@@ -3,17 +3,46 @@ Module dependencies.
 ###
 request = require 'request'
 
-module.exports = (clientId, userId = '') ->
+sendRequest = (url, callback) ->
+  request
+    .get url, callback
+
+setHeaders = (res) ->
+  res.setHeader "Content-Type", "application/json"
+  res.setHeader "Access-Control-Allow-Origin", "*"
+
+exports.firstPage = (clientId, userId = '') ->
   throw new Error '"clientId" parameter is required' if not clientId?
 
   return (req, res, next) ->
 
-    request
-      .get "https://api.instagram.com/v1/users/#{userId}/media/recent/?client_id=#{clientId}", (err, response, body) ->
 
-        res.setHeader "Content-Type", "application/json"
-        res.setHeader "Access-Control-Allow-Origin", "*"
+    sendRequest "https://api.instagram.com/v1/users/#{userId}/media/recent/?client_id=#{clientId}", (err, response, body) ->
+      setHeaders res
+      resObj = JSON.parse body
+      res.write JSON.stringify data: resObj.data
+      res.end()
 
-        res.write body
+exports.allPages = (clientId, userId = '') ->
+  throw new Error '"clientId" parameter is required' if not clientId?
 
+  return (req, res, next) ->
+
+    data = []
+
+    callback = (err, response, body) ->
+      resObj = JSON.parse body
+      paginationUrl = resObj.pagination['next_url']
+
+      data = data.concat resObj.data
+      console.log 'paginationurl:', paginationUrl
+
+      if paginationUrl?
+        sendRequest paginationUrl, callback
+      else
+        setHeaders res
+        console.log JSON.stringify data: data
+        res.write JSON.stringify data: data
         res.end()
+
+    sendRequest "https://api.instagram.com/v1/users/#{userId}/media/recent/?client_id=#{clientId}", callback
